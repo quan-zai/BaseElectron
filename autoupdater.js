36c5b5f3a170cefc13c25000d7fc6137e18d2209
徐正权 <xuzhengquan@yundianjia.com>
2018-02-06 09:58:53 +0800

    fix

--- autoupdater.js
+++ autoupdater.js
@@ -6,18 +6,23 @@ const platform = os.platform() + '_' + os.arch();  // usually returns darwin_64
 const updaterFeedURL = 'http://base-electron.herokuapp.com/update/' + platform + '/' + version;
 // replace updaterFeedURL with http://yourappname.herokuapp.com
 
-function appUpdater() {
+function appUpdater(mainWindow) {
     autoUpdater.setFeedURL(updaterFeedURL);
     /* Log whats happening
      TODO send autoUpdater events to renderer so that we could console log it in developer tools
      You could alsoe use nslog or other logging to see what's happening */
     autoUpdater.on('error', err => {
-        console.log(err)
-        ipcRenderer.send('console', err);
+        mainWindow.webContents.send('message', { msg: `😱 Error: ${err}` })
     });
-    autoUpdater.on('checking-for-update', () => console.log('checking-for-update'));
-    autoUpdater.on('update-available', () => console.log('update-available'));
-    autoUpdater.on('update-not-available', () => console.log('update-not-available'));
+    autoUpdater.on('checking-for-update', () => {
+        mainWindow.webContents.send('message', { msg: '🔎 Checking for updates' })
+    });
+    autoUpdater.on('update-available', () => {
+        mainWindow.webContents.send('message', { msg: '🎉 Update available. Downloading ⌛️', hide: false })
+    });
+    autoUpdater.on('update-not-available', () => {
+        mainWindow.webContents.send('message', { msg: '👎 Update not available' })
+    });
 
     // Ask the user if update is available
     autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
